fe24e51100adfb56c1167bf5ea2cb9755d17fd06
Peter YAU <9554866+petercyyau@users.noreply.github.com>
2025-01-09 09:42:49 +0800

    update

---
+++ lab01_ex_01.java
@@ -0,0 +1,18 @@
+/**
+ * Author: Dr. Peter Yau
+ * Email: PeterCY.Yau@glasgow.ac.uk
+ * 
+ * Copyright 2025
+ * School of Computing Science, University of Glasgow
+ *  
+ * Disclaimer:
+ * Programmers should ensure the environment and settings are correct.
+ * This code does not confirm 100% accuracy. Remember to change the filename and 
+ * class name to match your specific implementation.
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
